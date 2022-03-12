ab2dd7377415732a8cca1abf60b853355112851e
hugosenari <hugosenari@gmail.com>
2022-03-12 04:12:34 -0300

    chore: re index sling

---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''Signal and Slot library for Nim.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-sling-master.flake = false;
+  inputs.src-sling-master.owner = "Druage";
+  inputs.src-sling-master.ref   = "refs/heads/master";
+  inputs.src-sling-master.repo  = "sling";
+  inputs.src-sling-master.type  = "github";
+  
+  inputs."nimrod".dir   = "nimpkgs/n/nimrod";
+  inputs."nimrod".owner = "riinr";
+  inputs."nimrod".ref   = "flake-pinning";
+  inputs."nimrod".repo  = "flake-nimble";
+  inputs."nimrod".type  = "github";
+  inputs."nimrod".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimrod".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-sling-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-sling-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
